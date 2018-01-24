40bdded8e3fec39a95ddd5c14e0a8fed4b6ab127
Cody Short <nccgs117@gmail.com>
2018-01-24 14:56:10 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,11 @@
+function forLoop(array){
+  for (let i=0;i<25;i++){
+    if (i===1){
+      var string = "I am 1 strange loop."
+    } else {
+      string = `I am ${i} strange loops.`
+    }
+    array.push(string)
+  }
+  return array
+}
